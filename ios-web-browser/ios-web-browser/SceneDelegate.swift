516de274a26d8dfc58a4dfce5d821802af06575d
mauriciomaniglia <manigliamauricio@gmail.com>
2023-01-08 11:07:43 -0300

    Remove unused functions declaration in SceneDelegate.

--- ios-web-browser/ios-web-browser/SceneDelegate.swift
+++ ios-web-browser/ios-web-browser/SceneDelegate.swift
@@ -17,16 +17,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         mainController = mainViewController
     }
-
-    func sceneDidDisconnect(_ scene: UIScene) {}
-
-    func sceneDidBecomeActive(_ scene: UIScene) {}
-
-    func sceneWillResignActive(_ scene: UIScene) {}
-
-    func sceneWillEnterForeground(_ scene: UIScene) {}
-
-    func sceneDidEnterBackground(_ scene: UIScene) {}
 }
 
 extension SceneDelegate: MainViewControllerDelegate {
